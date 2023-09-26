7cdfc1b098c836b983d80f86d6727e34d6c53fd7
JuvenalUT <juvenal@urban-technology.net>
2023-09-26 12:29:58 +0100

    Update manager_by_api.py

--- manager_by_api.py
+++ manager_by_api.py
@@ -140,7 +140,28 @@ def user_cred(action=None):
 
             juser = jasmin.users(["get_creds", user])
 
-            dataUser = dict()
+            dataUser = dict(default_src_addr="",
+            quota_http_throughput="",
+            quota_balance="",
+            quota_smpps_throughput="",
+            quota_sms_count="",
+            quota_early_percent="",
+            value_priority="",
+            value_content="",
+            value_src_addr="",
+            value_dst_addr="",
+            value_validity_period="",
+            author_http_send="",
+            author_http_dlr_method="",
+            author_http_balance="",
+            author_smpps_send="",
+            author_priority="",
+            author_http_long_content="",
+            author_src_addr="",
+            author_dlr_level="",
+            author_http_rate="",
+            author_validity_period="",
+            author_http_bulk="")
 
             for j in juser[2:-1]:
                 r = str.split(j)
