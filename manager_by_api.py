6b2e828ad1e6ea8149ae7fa31abe1f3e6ab164dd
JuvenalUT <juvenal@urban-technology.net>
2023-11-02 19:25:16 +0100

    Update manager_by_api.py

--- manager_by_api.py
+++ manager_by_api.py
@@ -168,8 +168,7 @@ def get_order():
     return order
 
 def bj_mtrouter(data):
-    resp = data
-    resp["data"] = []
+    resp = []
     try:
         order = get_order()
         usr = data["usr"]
@@ -185,7 +184,7 @@ def bj_mtrouter(data):
             return dict(code=ret["code"], data=resp, message=ret["message"])
         
         data["filters"] = {"user":usr, "dest":'bj'}
-        # resp["data"].append(dict(data))
+        resp.append(dict(data))
 
         #################################
         
@@ -199,7 +198,7 @@ def bj_mtrouter(data):
             return dict(code=ret["code"], data=resp, message=ret["message"])
         
         data["filters"] = {"user":usr, "dest":'bj_moov'}
-        # resp["data"].append(dict(data))
+        resp.append(dict(data))
 
         #################################
         
@@ -213,7 +212,7 @@ def bj_mtrouter(data):
             return dict(code=ret["code"], data=resp, message=ret["message"])
         
         data["filters"] = {"user":usr, "dest":'bj_celtiis'}
-        # resp["data"].append(dict(data))
+        resp.append(dict(data))
     except Exception as e:
         return dict(code=400, data=resp, message=str(e))
     
