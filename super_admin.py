c8ae7559147a91e89f644a1d4606016ca997b4c0
JuvenalUT <juvenal@urban-technology.net>
2023-11-02 13:33:37 +0100

    Update super_admin.py

--- super_admin.py
+++ super_admin.py
@@ -175,7 +175,7 @@ def get_mtroutes():
         for f in f_split:
             f_type = ''
             f_val = ''
-            return dict(f)
+            return dict(k=f)
             if 'DA' in f:
                 f_type = 'DestinationAddrFilter'
                 matches = re.search(con_regex, f)
