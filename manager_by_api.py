ea548998184b9d980d3be0759978049e7a36a3c0
JuvenalUT <juvenal@urban-technology.net>
2023-11-02 16:46:08 +0100

    Update manager_by_api.py

--- manager_by_api.py
+++ manager_by_api.py
@@ -207,24 +207,27 @@ def filters_manage(usr=None,order=None,rate=None):
     if(not usr or not order or not rate):
         return api_resp(dict(user=usr, order=order, rate=rate), 404, "Invalid parameter")
     
-    resp = jasmin.mtrouter(['StaticMTRoute', order, 'smppc(bj_mtn)', usr+';bj;', rate])
-    if resp:
-        return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
-    
-    order = order + 1
-    resp = jasmin.mtrouter(['StaticMTRoute',order, 'smppc(bj_moov)', usr+';bj_moov;', rate])
-    if resp:
-        return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
-    
-    order = order + 1
-    resp = jasmin.mtrouter(['StaticMTRoute',order, 'smppc(bj_moov)', usr+';bj_celtiis;', rate])
-    if resp:
-        return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
-    
     try:
-        api_popualate_database()
+        # resp = jasmin.mtrouter(['StaticMTRoute', order, 'smppc(bj_mtn)', usr+';bj;', rate])
+        # if resp:
+        #     return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
+        
+        order = order + 1
+        resp = jasmin.mtrouter(['StaticMTRoute',order, 'smppc(bj_moov)', usr+';bj_moov;', rate])
+        if resp:
+            return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
+        
+        order = order + 1
+        resp = jasmin.mtrouter(['StaticMTRoute',order, 'smppc(bj_moov)', usr+';bj_celtiis;', rate])
+        if resp:
+            return api_resp(dict(user=usr, order=order, rate=rate), 400, resp)
+        
+        try:
+            api_popualate_database()
+        except Exception as e:
+            message=str(e)
     except Exception as e:
-        message=str(e)
+        return api_resp(dict(user=usr, order=order, rate=rate), 400, str(e))
     
     return api_resp(dict(user=usr, order=order, rate=rate), 200, "Adds mt routers")
 
