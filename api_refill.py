7e7fcd49d6de83eac39b159c9b907d125d735f17
JuvenalUT <juvenal@urban-technology.net>
2023-11-20 16:29:40 +0100

    Update api_refill.py

--- api_refill.py
+++ api_refill.py
@@ -76,7 +76,53 @@ def refill(data, cred=None):
     
     return dict(code=200, balance=cred["quota_balance"], message='User credentials updated')
 
-@action('api/refill', method=['GET', 'POST'])
+def replace(data):
+    try:
+        user = data["uid"]
+        balance = data["balance"]
+
+        cred = getCreds(user)
+
+        if cred == {}:
+            return dict(code=404, message="User not found")
+
+        if not balance == None:
+            cred["quota_balance"] = balance
+        
+        cred["quota_balance"] = str(cred["quota_balance"])
+            
+        ret = jasmin.users(['update', user,
+            cred["default_src_addr"],
+            cred["quota_http_throughput"],
+            cred["quota_balance"],
+            cred["quota_smpps_throughput"],
+            cred["quota_sms_count"],
+            cred["quota_early_percent"],
+            cred["value_priority"],
+            cred["value_content"],
+            cred["value_src_addr"],
+            cred["value_dst_addr"],
+            cred["value_validity_period"],
+            cred["author_http_send"],
+            cred["author_http_dlr_method"],
+            cred["author_http_balance"],
+            cred["author_smpps_send"],
+            cred["author_priority"],
+            cred["author_http_long_content"],
+            cred["author_src_addr"],
+            cred["author_dlr_level"],
+            cred["author_http_rate"],
+            cred["author_validity_period"],
+            cred["author_http_bulk"]])
+
+        if ret:
+            return dict(code=403, message=ret)
+    except Exception as e:
+        return dict(code=403, message=str(e))
+    
+    return dict(code=200, balance=cred["quota_balance"], message='User credentials updated')
+
+@action('api/refill/<action>', method=['GET', 'POST'])
 @action.uses(db, session, auth, flash)
 def refill_manage(action=None):
     ret = api_id(request)
@@ -86,7 +132,10 @@ def refill_manage(action=None):
     data = request.POST
 
     try:
-        ret = refill(data)
+        if action == "add":
+            ret = refill(data)
+        elif action == "replace":
+            ret = replace(data)
     except Exception as e:
         return api_resp(dict(request.POST), 400, str(e))
 
