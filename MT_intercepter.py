6b8406db4dfb9fa65630fc68d19dd5a76a10b021
JuvenalUT <juvenal@urban-technology.net>
2024-10-02 21:29:04 +0100

    Update MT_intercepter.py

--- MT_intercepter.py
+++ MT_intercepter.py
@@ -2,12 +2,66 @@
 
 import json
 import requests
-import datetime
+from datetime import datetime
 
+EXTERNAL_API_URL = "https://edok-api.kingsmspro.com/api/v1/sms/send"
+API_KEY = 'NgDnKzjDpv3EndwGiOrVBBLHDivERcZt'  # clé API
+CLIENT_ID = "2839"  #  client ID
+
+def send_sms_via_api(from_, to, message, type_, dlr):
+    """Envoie le SMS via l'API externe."""
+    try:
+        sms_data = {
+            'from': from_,
+            'to': to,
+            'type': type_,
+            'message': message,
+            'dlr': dlr
+        }
+
+        headers = {
+            "APIKEY": API_KEY,
+            "CLIENTID": CLIENT_ID
+        }
+
+        # Envoi du SMS via l'API
+        response = requests.post(EXTERNAL_API_URL, data=sms_data, headers=headers)
+
+        # Vérification du statut de la réponse
+        if response.status_code == 200:
+            return True, response.json()  # Réponse OK
+        else:
+            return False, response.text  # Réponse en erreur
+    except Exception as e:
+        #sys.stderr.write(f"Erreur d'envoi de l'API : {str(e)}\n")
+        return False, str(e)
+    
+globals()['json'] = json
 try:
-    routable
-    smpp_status = "ESME_ROK"
-    http_status = 200
+    dateSend = datetime.now().isoformat() + 'Z'
+    to = routable.pdu.params['destination_addr']
+    sender = routable.pdu.params['source_addr']
+    content = routable.pdu.params['short_message']
+    message_type = 'text'  # Type de message (par défaut 'text')
+    dlr = '1'  # DLR (par défaut 1 pour accusé de réception)
+
+    # Envoi du SMS via l'API HTTP externe
+    success, api_response = send_sms_via_api(sender, to, content, message_type, dlr)
 except Exception as e:
-    smpp_status = "ESME_RCANCELFAIL"
-    http_status = 400
+    # We got an error when calling for charging
+    # Return ESME_RDELIVERYFAILURE
+    smpp_status = 254
+else:
+    # CGRateS has returned a value
+
+    if success:
+        # Return ESME_ROK
+        smpp_status = 0
+    else:
+        # Return ESME_RDELIVERYFAILURE
+        smpp_status = 254
+finally:
+    log_file = "/var/log/jasmin/mt_interceptor.log"
+    with open(log_file, "a") as file:
+        file.write("{0} : send SMS from {1} to {2} ({3}-{4})".format(dateSend, sender, to, success, api_response))
+        file.write('\n')
