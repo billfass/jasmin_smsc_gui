7213dcb4d77bfd74ed35a72d02495232e44d834a
JuvenalUT <juvenal@urban-technology.net>
2024-12-19 12:02:14 +0100

    Update intercepter_rouch.py

--- intercepter_rouch.py
+++ intercepter_rouch.py
@@ -3,23 +3,19 @@
 import json
 import re
 
+pattern = r"^229(01)?(55|58|6[03-58]|9[3-589])[0-9]{6}$"
+
 globals()['json'] = json
 globals()['re'] = re
+globals()['pattern'] = pattern
 try:
-    m_user = "FAST_6924" # routable.user
+    m_user = routable.user
     to = routable.pdu.params['destination_addr']
     sender = routable.pdu.params['source_addr']
     content = routable.pdu.params['short_message']
     log_file = "/var/log/jasmin/rouch_intercepter.log"
 
     api_text = json.dumps({"m_user": m_user, "to":to, "sender":sender, "message": "Send SMS"})
-
-    with open(log_file, "a") as file:
-        file.write("{0}".format(api_text))
-        file.write('\n')
-
-    # Définir le motif
-    pattern = r"^229(01)?(55|58|6[03-58]|9[3-589])[0-9]{6}$"
 
     # Exemple de fonction pour valider
     def match_number(number):
