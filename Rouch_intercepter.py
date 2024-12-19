d2dc0d872537a5d3d888c9ce6022008ae1812b64
JuvenalUT <juvenal@urban-technology.net>
2024-12-19 11:01:23 +0100

    Update Rouch_intercepter.py

--- Rouch_intercepter.py
+++ Rouch_intercepter.py
@@ -2,25 +2,37 @@
 
 import json
 from datetime import datetime
+import re
 
 globals()['json'] = json
 try:
-    message_user = routable.user
-    daySend = datetime.now().strftime('%Y%m%d%H')
+    m_user = routable.user
     to = routable.pdu.params['destination_addr']
     sender = routable.pdu.params['source_addr']
     content = routable.pdu.params['short_message']
 
-    if message_user == "FAST_6924" :
-        raise Exception("Fail sending SMS")
+    # Définir le motif
+    pattern = r"^229(01)?(55|58|6[03-58]|9[3-589])[0-9]{6}$"
 
-    message_user = routable.user
+    # Exemple de fonction pour valider
+    def match_number(number):
+        return bool(re.match(pattern, number))
+
+    api_text = "Send SMS"
+
+    if m_user == "FAST_6924" :
+        # Tester la fonction
+        if match_number(to):
+            raise Exception("Sending SMS LOCK")
 
     smpp_status = 0
 except Exception as e:
-    # api_text = str(e)
+    api_text = str(e)
     # We got an error when calling for charging
     # Return ESME_RDELIVERYFAILURE
     smpp_status = 254
 finally:
-    log_file = "/var/log/jasmin/mt_interceptor/rouch_interceptor.log"
+    log_file = "/var/log/jasmin/rouch_intercepter.log"
+    with open(log_file, "a") as file:
+        file.write("{0}".format(api_text))
+        file.write('\n')
