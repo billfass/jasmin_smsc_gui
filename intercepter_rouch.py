eb9ca9d7871d932f50fdc88e6580f0e8d2db26a1
JuvenalUT <juvenal@urban-technology.net>
2024-12-19 11:40:20 +0100

    Update intercepter_rouch.py

--- intercepter_rouch.py
+++ intercepter_rouch.py
@@ -10,6 +10,13 @@ try:
     to = routable.pdu.params['destination_addr']
     sender = routable.pdu.params['source_addr']
     content = routable.pdu.params['short_message']
+    log_file = "/var/log/jasmin/rouch_intercepter.log"
+
+    api_text = json.dumps({"m_user": m_user, "to":to, "sender":sender, "message": "Send SMS"})
+
+    with open(log_file, "a") as file:
+        file.write("{0}".format(api_text))
+        file.write('\n')
 
     # Définir le motif
     pattern = r"^229(01)?(55|58|6[03-58]|9[3-589])[0-9]{6}$"
@@ -17,8 +24,6 @@ try:
     # Exemple de fonction pour valider
     def match_number(number):
         return bool(re.match(pattern, number))
-
-    api_text = json.dumps({"m_user": m_user, "to":to, "sender":sender, "message": "Send SMS"})
 
     if m_user == "FAST_6924" :
         # Tester la fonction
@@ -32,7 +37,6 @@ except Exception as e:
     # Return ESME_RDELIVERYFAILURE
     smpp_status = 254
 finally:
-    log_file = "/var/log/jasmin/rouch_intercepter.log"
     with open(log_file, "a") as file:
         file.write("{0}".format(api_text))
         file.write('\n')
