325fa33a7d3984a347048d1eb7293a787727ac1c
JuvenalUT <juvenal@urban-technology.net>
2025-03-17 16:51:30 +0100

    Update api_groups.py

--- api_groups.py
+++ api_groups.py
@@ -2,7 +2,7 @@ from py4web import action, request
 from .common import db, session, auth, flash, jasmin, api_resp, api_id
 from pydal.validators import *
 from .utils import cols_split
-from .user_manager import list_groups
+from .user_manager import list_groups, remove_group
 from .api_filters import new_filter
 from .api_mtrouters import bj_routers_by_group
 from .super_admin import api_popualate_database
@@ -23,6 +23,21 @@ def new_group(data):
     
     return dict(code=200, message='Added group %s' %data)
 
+def restore_group(data):
+    try:
+        for gr in list_groups():
+            remove_group(gr["gid"])
+
+        for gr in data:
+            ret=jasmin.users(['create_group', gr["gid"]])
+            if ret:
+                return dict(code=400, message=ret)
+
+    except Exception as e:
+        return dict(code=403, message=str(e))
+    
+    return dict(code=200, message='Restore groups %s' %data)
+
 @action('api/groups/<action>', method=['GET', 'POST'])
 @action.uses(db, session, auth, flash)
 def groups_manage(action=None):
@@ -35,6 +50,8 @@ def groups_manage(action=None):
     try:
         if action == "create":
             ret = new_group(data)
+        elif action == "restore":
+            return dict(code=200, message='Restore groups %s' %data)
         elif action == "list":
             return api_resp(list_groups(), 200, "Group's user")
         else:
