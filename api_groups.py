ce86de45009a7d09722fc5efdf54ac12835ca0a3
JuvenalUT <juvenal@urban-technology.net>
2025-03-18 10:22:54 +0100

    Update api_groups.py

--- api_groups.py
+++ api_groups.py
@@ -52,7 +52,7 @@ def groups_manage(action=None):
             data = request.POST
             ret = new_group(data)
         elif action == "restore":
-            data = request.json
+            # data = request.json
             try:
                 # Lire le contenu brut du body (sinon, request.json ne fonctionnera pas après)
                 raw_body = request.body.read().decode("utf-8")  
