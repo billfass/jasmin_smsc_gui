0040182be19e969f78614f75e1cf94cec35f2f1c
JuvenalUT <juvenal@urban-technology.net>
2025-03-18 09:13:36 +0100

    Update api_groups.py

--- api_groups.py
+++ api_groups.py
@@ -55,18 +55,18 @@ def groups_manage(action=None):
             # data = request.json
             try:
                 # Lire le contenu brut du body (sinon, request.json ne fonctionnera pas après)
-                raw_body = request.body.read().decode("utf-8")  
+                # raw_body = request.body.read().decode("utf-8")  
 
                 # Re-parsing du JSON (car après .read(), request.json sera None)
-                parsed_json = json.loads(raw_body) if raw_body else None  
+                # parsed_json = json.loads(raw_body) if raw_body else None  
 
                 # Retourner les informations en JSON
                 return {
                     "status": "success",
                     # "data": json.loads(data),
                     "headers": dict(request.headers),  # Convertir les headers en dictionnaire
-                    "raw_body": raw_body,
-                    "parsed_json": parsed_json
+                    # "raw_body": raw_body,
+                    # "parsed_json": parsed_json
                 }
             except Exception as e:
                 return {"status": "error", "message": str(e)}
