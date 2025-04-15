5414b90367a1bcb3e9424b028ede9f6e3ad19e7d
JuvenalUT <juvenal@urban-technology.net>
2025-04-15 12:30:42 +0100

    Update intercepter_sid_fastermsg.py

--- intercepter_sid_fastermsg.py
+++ intercepter_sid_fastermsg.py
@@ -12,12 +12,13 @@ try:
     to = routable.pdu.params['destination_addr']
     sender = routable.pdu.params['source_addr']
     content = routable.pdu.params['short_message']
+    sid = 'OTPCode'
 
     if sender == 'FASTERMSG':
-        routable.pdu.params['source_addr'] = 'OTPCode'
+        routable.pdu.params['source_addr'] = sid
         raise Exception("SID FASTERMSG getting")
 except Exception as e:
-    api_text = json.dumps({"m_user": user, "to":to, "sender":sender, "message": str(e), "time": datetime.datetime.now().strftime("%Y%m%d%H%i%s")})
+    api_text = json.dumps({"m_user": user, "to":to, "sender":sender, "sid_final":sid, "message": str(e), "time": datetime.datetime.now().strftime("%Y%m%d%H%i%s")})
     # We got an error when calling for charging
     with open("{0}_{1}.log".format(log_file, today), "a") as file:
         file.write("{0}".format(api_text))
