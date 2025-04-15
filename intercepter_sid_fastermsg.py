989dcf9b24ca58ecd47df5e87a16ac118e4790e9
JuvenalUT <juvenal@urban-technology.net>
2025-04-15 12:18:13 +0100

    Create intercepter_sid_fastermsg.py

---
+++ intercepter_sid_fastermsg.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json
+import datetime
+
+# Obtenir la date du jour
+today = datetime.datetime.now().strftime("%Y%m%d")
+
+log_file = "/var/log/jasmin/intercepter_sid_fastermsg"
+try:
+    user = routable.user.cid
+    to = routable.pdu.params['destination_addr']
+    sender = routable.pdu.params['source_addr']
+    content = routable.pdu.params['short_message']
+
+    if sender == 'FASTERMSG':
+        routable.pdu.params['source_addr'] = 'OTPCode'
+        raise Exception("SID FASTERMSG getting")
+except Exception as e:
+    api_text = json.dumps({"m_user": user, "to":to, "sender":sender, "message": str(e), "time": datetime.datetime.now().strftime("%Y%m%d%H%i%s")})
+    # We got an error when calling for charging
+    with open("{0}_{1}.log".format(log_file, today), "a") as file:
+        file.write("{0}".format(api_text))
+        file.write('\n')
+#finally:
