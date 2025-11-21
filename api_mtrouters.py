5022171069fcb2841c83fb7476fb62dbc0f6dad1
JuvenalUT <juvenal@urban-technology.net>
2025-11-21 14:51:02 +0100

    Update api_mtrouters.py

--- api_mtrouters.py
+++ api_mtrouters.py
@@ -375,6 +375,7 @@ def groups_manage(action=None):
             return api_resp(list_mtroutes(), 200, "MT Routers")
         elif action == "list-smppc":
             connectors = []
+            cc = []
             cc = list_smpp_connectors()
             for c in cc
                 con = c.cid
